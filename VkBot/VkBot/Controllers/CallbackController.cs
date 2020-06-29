f22e994bd9052468ef200132bbb88af5327e8fdd
Dan <dan.mok999@yandex.ru>
2020-06-29 19:26:32 +0500

    MVP

---
+++ VkBot/VkBot/Controllers/CallbackController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using VkBot.Models;
+using VkNet.Abstractions;
+using VkNet.Model;
+using VkNet.Model.RequestParams;
+using VkNet.Utils;
+
+namespace VkBot.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CallbackController : ControllerBase
+	{
+		/// <summary>
+		/// Конфигурация приложения
+		/// </summary>
+		private readonly IConfiguration configuration;
+
+		private readonly IVkApi vkApi;
+
+		public CallbackController(IConfiguration configuration, IVkApi vkApi)
+		{
+			this.configuration = configuration;
+			this.vkApi = vkApi;
+		}
+
+		[HttpPost]
+		public IActionResult Callback([FromBody] Updates updates)
+		{
+			// Проверяем, что находится в поле "type" 
+			switch (updates.Type)
+			{
+				// Если это уведомление для подтверждения адреса
+				case "confirmation":
+					// Отправляем строку для подтверждения 
+					return Ok(configuration["Config:Confirmation"]);
+				case "message_new":
+					// Десериализация
+					var msg = Message.FromJson(new VkResponse(updates.Object));
+
+					// Отправим в ответ полученный от пользователя текст
+					vkApi.Messages.Send(new MessagesSendParams
+					{
+						RandomId = new DateTime().Millisecond,
+						PeerId = msg.PeerId,
+						Message = msg.Text
+					});
+					break;
+			}
+			// Возвращаем "ok" серверу Callback API
+			return Ok("ok");
+		}
+	}
+}
--- VkBot/VkBot/Startup.cs
+++ VkBot/VkBot/Startup.cs
@@ -4,18 +4,36 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using VkNet;
+using VkNet.Abstractions;
+using VkNet.Model;
 
 namespace VkBot
 {
 	public class Startup
 	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
 		// This method gets called by the runtime. Use this method to add services to the container.
-		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddSingleton<IVkApi>(sp => {
+				var api = new VkApi();
+				api.Authorize(new ApiAuthParams{ AccessToken = Configuration["Config:AccessToken"] });
+				return api;
+			});
+			services.AddControllers();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,12 +44,13 @@ namespace VkBot
 				app.UseDeveloperExceptionPage();
 			}
 
+			app.UseHttpsRedirection();
+
 			app.UseRouting();
 
-			app.UseEndpoints(endpoints =>
-			{
-				endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
-			});
+			app.UseAuthorization();
+
+			app.UseEndpoints(endpoints => endpoints.MapControllers());
 		}
 	}
 }
