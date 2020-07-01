5b9529f1478e5efba6065c10976263078f918ba3
Dan <dan.mok999@yandex.ru>
2020-07-01 18:15:56 +0500

    check server working

---
+++ VkBot/VkBot/Controllers/Home.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace VkBot.Controllers
+{
+	[Route("")]
+	public class Home : Controller
+	{
+		private readonly IConfiguration configuration;
+
+		public Home(IConfiguration configuration)
+		{
+			this.configuration = configuration;
+		}
+		
+		[HttpGet]
+		public ActionResult GetConfirmCode()
+		{
+			return Ok(configuration["Config:Confirmation"]);
+		}
+	}
+}
