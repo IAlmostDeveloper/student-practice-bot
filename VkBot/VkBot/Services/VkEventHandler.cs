49a8bc51f5c871c85de46e0f98ab4e0af0b313fa
Ialmostdeveloper <kill2survive@yandex.ru>
2020-07-03 07:12:28 +0300

    Bot response from database

--- VkBot/VkBot/Services/VkEventHandler.cs
+++ VkBot/VkBot/Services/VkEventHandler.cs
@@ -10,45 +10,47 @@ using VkNet.Utils;
 
 namespace VkBot.Services
 {
-	public class VkEventHandler
-	{
-		private readonly IVkApi vkApi;
-		private readonly QuestionAndAnswerRepository questionAndAnswerRepository;
+    public class VkEventHandler
+    {
+        private readonly IVkApi vkApi;
+        private readonly QuestionAndAnswerRepository questionAndAnswerRepository;
 
-		public VkEventHandler(IVkApi vkApi, QuestionAndAnswerRepository questionAndAnswerRepository)
-		{
-			this.vkApi = vkApi;
-			this.questionAndAnswerRepository = questionAndAnswerRepository;
-		}
+        public VkEventHandler(IVkApi vkApi, QuestionAndAnswerRepository questionAndAnswerRepository)
+        {
+            this.vkApi = vkApi;
+            this.questionAndAnswerRepository = questionAndAnswerRepository;
+        }
 
-		public void MessageNew(VkResponse vkResponse)
-		{
-			var message = Message.FromJson(vkResponse);
-			var messagesSendParams = new MessagesSendParams
-			{
-				RandomId = new DateTime().Millisecond,
-				PeerId = message.PeerId,
-				Message = message.Text
-			};
-			vkApi.Messages.Send(messagesSendParams);
-		}
-		
-		public void GroupJoin(VkResponse vkResponse)
-		{
-			var groupJoin = VkNet.Model.GroupUpdate.GroupJoin.FromJson(vkResponse);
-			var userName = "Человек";
-			if (groupJoin.UserId != null)
-			{
-				var user = vkApi.Users.Get(new[] {groupJoin.UserId.Value}).FirstOrDefault();
-				userName = user != null ? user.FirstName : userName;
-			}
-			var messageParams = new MessagesSendParams
-			{
-				PeerId = groupJoin.UserId,
-				RandomId = new DateTime().Millisecond,
-				Message = $"Hello {userName} :3"
-			};
-			vkApi.Messages.Send(messageParams);
-		}
-	}
+        public void MessageNew(VkResponse vkResponse)
+        {
+            var message = Message.FromJson(vkResponse);
+            var response = questionAndAnswerRepository.FindByQuestion(message.Text);
+            var messagesSendParams = new MessagesSendParams
+            {
+                RandomId = new DateTime().Millisecond,
+                PeerId = message.PeerId,
+                Message = response != null ? response.Answer : "Ваш вопрос не найден в моей БД"
+            };
+            vkApi.Messages.Send(messagesSendParams);
+        }
+
+        public void GroupJoin(VkResponse vkResponse)
+        {
+            var groupJoin = VkNet.Model.GroupUpdate.GroupJoin.FromJson(vkResponse);
+            var userName = "Человек";
+            if (groupJoin.UserId != null)
+            {
+                var user = vkApi.Users.Get(new[] {groupJoin.UserId.Value}).FirstOrDefault();
+                userName = user != null ? user.FirstName : userName;
+            }
+
+            var messageParams = new MessagesSendParams
+            {
+                PeerId = groupJoin.UserId,
+                RandomId = new DateTime().Millisecond,
+                Message = $"Hello {userName} :3"
+            };
+            vkApi.Messages.Send(messageParams);
+        }
+    }
 }
