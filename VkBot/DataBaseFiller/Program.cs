8a64cd6f233743aea3db5727e6fb2dfcd2f06fc4
Ialmostdeveloper <kill2survive@yandex.ru>
2020-07-04 16:48:23 +0300

    new db

--- VkBot/DataBaseFiller/Program.cs
+++ VkBot/DataBaseFiller/Program.cs
@@ -9,12 +9,12 @@ namespace DBFiller
     {
         static void Main(string[] args)
         {
-            var connectionString = "Server=localhost;Port=3306;User=root;Password=thereisnospoon;Database=mydb;";
+            var connectionString = "Server=localhost;Port=3306;User=u1090_113default;Password=E51wd#2i;Database=u1090113_default;";
 
             var db = new MySqlDataBase(connectionString);
             var parser = new CSVParser("csvData.csv",
                 new QuestionAndAnswerRepository(db), new WordAndAnswerRepository(db));
-            parser.Parse();
+            // parser.Parse();
             var qr = new QuestionAndAnswerRepository(db);
             var answers = new WordAndAnswerRepository(db).FindSeveralByPhrase("что курсовая когда зачем баллы");
             foreach (var answer in qr.FindSeveralByAnswers(answers))
